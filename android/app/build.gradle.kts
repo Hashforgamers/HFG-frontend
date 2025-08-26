7de94ef48dd6016cae04af6f5e6fed89017ed61c
zeyan-ansari <zeyanansari10@gmail.com>
2025-08-27 00:18:54 +0530

    minor optimziations

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,6 +9,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("org.jetbrains.kotlin.android")
 }
 
 /* ───────── load external properties ───────── */
@@ -94,8 +95,13 @@ android {
 dependencies {
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     implementation("com.guardsquare:proguard-annotations:7.4.1")
-
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
     // add other libs below …
+    // Needed for Theme.Material3.*
+    implementation("com.google.android.material:material:1.12.0")
+// Needed for Theme.SplashScreen + windowSplashScreen* attrs (Android 12 compat)
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
 
 /* ───────── flutter source ───────── */
